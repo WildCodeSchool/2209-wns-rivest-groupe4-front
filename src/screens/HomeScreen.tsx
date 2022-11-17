964c5bdb09a8714070c5d5d52ad42e2f165ce853
Quentin Billac <quentin.billac2@gmail.com>
2022-11-17 15:14:53 +0100

    add function login and token

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const HomeScreen = () => {
+
+  const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token === null) {
+      navigate("/login");
+    }
+  });
+
+  return (
+    <div>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import LoginContainer from '../container/LoginContainer';
+
+const LoginScreen = () => {
+  return (
+    <section>
+        <h1>Login screen</h1>
+        <LoginContainer />
+    </section>
+  )
+}
+
+export default LoginScreen
