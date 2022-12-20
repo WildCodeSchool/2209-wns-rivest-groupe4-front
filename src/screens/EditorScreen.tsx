ddfe6571b5d44a7a2b195143475f0990cc745b91
Joeri Kaiser <joeri.kaiser@medelse.com>
2022-12-20 11:55:53 +0100

    Added AceEditor and integrated it into the EditorScreen

--- src/screens/EditorScreen.tsx
+++ src/screens/EditorScreen.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import AceEditor from "react-ace";
 
 function EditorScreen() {
-  return <div>EditorScreen</div>;
+  return (
+    <div>
+      <AceEditor
+        mode="javascript"
+        theme="github"
+        // TODO create function to handle behaviour
+        onChange={() => {}}
+        onCopy={() => {}}
+        onPaste={() => {}}
+        onBlur={() => {}}
+        onValidate={() => {}}
+        name="code_editor"
+        placeholder="Codeless 4 Editor"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true,
+        }}
+        className="main-editor"
+        showGutter
+        showPrintMargin
+        style={{ borderWidth: 1, borderColor: "red" }}
+      />
+    </div>
+  );
 }
 
 export default EditorScreen;
