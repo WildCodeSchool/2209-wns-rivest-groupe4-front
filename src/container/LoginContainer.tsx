080f4df01501c724e3248af517bd6b5530dd9645
Derma6 <dermaweb.dev@gmail.com>
2022-12-21 16:22:36 +0100

    waiting PR T39 to have flowbite-react

--- src/container/LoginContainer.tsx
+++ src/container/LoginContainer.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { Button } from "flowbite-react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { gql, useLazyQuery } from "@apollo/client";
 
 const GET_TOKEN = gql`
@@ -13,7 +14,7 @@ function LoginContainer() {
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
-  const [loadToken, { loading, error }] = useLazyQuery(GET_TOKEN, {
+  const [loadToken, { error }] = useLazyQuery(GET_TOKEN, {
     onCompleted(data: { getToken: string }) {
       localStorage.setItem("token", data.getToken);
       navigate("/");
@@ -53,7 +54,7 @@ function LoginContainer() {
       <div className="mb-6">
         <label
           htmlFor="password"
-          className="block mb-2 text-lg font-medium  dark:text-white"
+          className="block text-lg font-medium  dark:text-white"
         >
           Password
           <input
@@ -80,24 +81,26 @@ function LoginContainer() {
             Email or password invalid
           </span>
         )}
+        <Link to="/login" className="text-sm block">
+          Password forgotten
+        </Link>
       </div>
       <div className="mb-6">
         <label
           htmlFor="remember"
-          className="ml-2 text-lg font-medium dark:text-gray-300"
+          className="text-lg font-medium dark:text-gray-300"
         >
-          Remember me
           <input
             id="remember"
             type="checkbox"
             value=""
-            className="w-4 h-4 ml-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+            className="w-4 h-4 mr-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
             required
           />
+          Remember me
         </label>
       </div>
-      <button
-        type="submit"
+      {/* <Button
         onClick={() => {
           loadToken({
             variables: {
@@ -106,29 +109,16 @@ function LoginContainer() {
             },
           });
         }}
-        className="text-white bg-gradient-to-r from-blue-500 via-[#131d2f] to-[#1d2448] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center block m-auto mb-2"
+        gradientDuoTone="cyanToBlue"
       >
-        {loading && (
-          <svg
-            aria-hidden="true"
-            role="status"
-            className="inline mr-3 w-4 h-4 text-white animate-spin"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="#E5E7EB"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
-            />
-          </svg>
-        )}
-        SIGN IN
-      </button>
+        SIGN UP
+      </Button> */}
+      <p className="text-center text-lg mt-5">
+        <NavLink to="/register" className="underline">
+          Sign up
+        </NavLink>{" "}
+        if you don&apos;t have an account yet !
+      </p>
     </div>
   );
 }
