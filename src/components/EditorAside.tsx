f479018ab0c84e419943554eaa670f3896b0c2ac
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-02-02 10:32:30 +0100

    Created route for choice screen and created tree structure for project inside the aside editor

--- src/components/EditorAside.tsx
+++ src/components/EditorAside.tsx
@@ -1,6 +1,84 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
+import {
+  ExistingProjectQueryResult,
+  FolderTree,
+} from "../container/EditorContainer/types";
 
-function EditorAside() {
+interface Props {
+  projectData: ExistingProjectQueryResult;
+  setCurrentFileContent: (content: string) => void;
+}
+
+function Folder({
+  folder,
+  depth,
+  setCurrentFileContent,
+}: {
+  folder: FolderTree;
+  depth: number;
+  setCurrentFileContent: (content: string) => void;
+}) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  return (
+    <div style={{ marginLeft: depth * 20, cursor: "pointer" }}>
+      <div onClick={() => setIsCollapsed(!isCollapsed)}>
+        {folder.name} {!isCollapsed ? "-" : "+"}
+      </div>
+      {!isCollapsed &&
+        folder.files.map((file) => (
+          <div
+            onClick={() => setCurrentFileContent(file.content)}
+            key={`${file.content}-${file.name}-${file.extension}`}
+            style={{ marginLeft: 20 }}
+          >
+            {`${file.name}.${file.extension}`}
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function Tree({
+  tree,
+  setCurrentFileContent,
+}: {
+  tree: FolderTree[];
+  setCurrentFileContent: (content: string) => void;
+}) {
+  return (
+    <div>
+      {tree.map((folder, index) => (
+        <Folder
+          setCurrentFileContent={setCurrentFileContent}
+          key={folder.name}
+          folder={folder}
+          depth={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+const createTree = (
+  folders: Array<FolderTree>,
+  parentFolderId: string | null = null,
+): FolderTree[] => {
+  return folders
+    .filter((folder) => folder.parentFolder === parentFolderId)
+    .map((folder) => {
+      const children = createTree(folders, folder.id);
+      return {
+        name: folder.name,
+        files: folder.files,
+        children,
+      };
+    });
+};
+
+function EditorAside({ projectData, setCurrentFileContent }: Props) {
   return (
     <aside
       className="w-[10%] bg-[#20252D]"
@@ -9,8 +87,10 @@ function EditorAside() {
         borderColor: "white",
       }}
     >
-      {/* TODO add dynamic project name */}
-      <p>Project</p>
+      <Tree
+        setCurrentFileContent={setCurrentFileContent}
+        tree={createTree(projectData.getAllFoldersByProjectId)}
+      />
     </aside>
   );
 }
