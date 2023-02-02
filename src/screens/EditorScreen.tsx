c0b4b8ae5f417c5c7eb3b3bc6fc65cf208dda06d
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-02-02 10:34:08 +0100

    Created choose screen as well as modified the editor screen

--- src/screens/EditorScreen.tsx
+++ src/screens/EditorScreen.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import EditorContainer from "../container/EditorContainer";
+import { gql, useQuery } from "@apollo/client";
+import { useQueryParams } from "../utils/utils";
+import EditorContainer from "../container/EditorContainer/EditorContainer";
+
+const GET_PROJECTS = gql`
+  query GetProjectsByUserId($userId: String!) {
+    getProjectsByUserId(userId: $userId) {
+      name
+      id
+    }
+  }
+`;
 
 function EditorScreen() {
-  return <EditorContainer />;
+  const query = useQueryParams();
+  const { loading, error, data } = useQuery(GET_PROJECTS, {
+    // TODO use userId
+    variables: { userId: "930a55eb-73ec-4a96-8f83-5dd8552831e8" },
+  });
+  if (error) {
+    return <div>Error</div>;
+  }
+  if (loading) {
+    return <div>Loading</div>;
+  }
+  return (
+    <EditorContainer
+      action={query.get("open")}
+      existingProjects={data.getProjectsByUserId}
+    />
+  );
 }
 
 export default EditorScreen;
